b1b6963386793e724d1d9f17e1a3fa6588641a8e
mossfreestyle <mosswassims3@gmail.com>
2025-12-19 20:21:33 +0100

    ex04

---
+++ ex04/Whatis.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python3
+
+import sys
+
+def is_int(nb):
+    try:
+        int(nb)
+        return True
+    except ValueError:
+        return False
+
+try:
+    if len(sys.argv) == 1:
+        exit()
+    elif len(sys.argv) > 2:
+        raise AssertionError("more than one argument is provided")
+    elif not is_int(sys.argv[1]):
+        raise AssertionError("argument is not an int")
+    else:
+        nbr = int(sys.argv[1])
+        print("I'm Even." if nbr % 2 == 0 else "I'm Odd.")
+
+except AssertionError as e:
+    print(f"AssertionError: {e}")
